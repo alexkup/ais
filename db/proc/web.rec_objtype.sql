4c0aa87195449f08993d4626a30327761236b7f5
alexkup <alexkup@mail.ru>
2014-07-15 07:23:04 +0400

    Create web.rec_objtype.sql

---
+++ db/proc/web.rec_objtype.sql
@@ -0,0 +1,17 @@
+create or replace procedure web.rec_objtype()
+begin
+declare @body long nvarchar;
+set @body=http_body();
+set @body=left(@body,length(@body)-1);
+set @body=right(@body,length(@body)-1);
+truncate table web.obj_type_1c;
+insert into web.obj_type_1c
+SELECT
+    name ,link,xid   
+FROM OPENSTRING(value @body) 
+WITH (name nvarchar(255),link nvarchar(255),xid UNIQUEIDENTIFIER)
+option(delimited by '|') 
+as h;
+select 'OK';
+end;
+CREATE SERVICE "objtype" TYPE 'RAW' AUTHORIZATION OFF USER "web" AS call web.rec_objtype();
