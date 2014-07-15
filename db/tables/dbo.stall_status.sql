3849b4c08822a31da2a6c0b817ff25bed1e43cd7
alexkup <alexkup@mail.ru>
2014-07-15 07:15:19 +0400

    Update dbo.stall_status.sql

--- db/tables/dbo.stall_status.sql
+++ db/tables/dbo.stall_status.sql
@@ -6,3 +6,6 @@ CREATE TABLE dbo.stall_status (
 	PRIMARY KEY ("id" ASC)		
 ) IN "system";
 COMMENT ON TABLE "dbo"."stall_status" IS 'Состояние мест';
+
+ALTER TABLE "dbo"."stall_status" ADD CONSTRAINT "stalls" NOT NULL FOREIGN KEY ( "id_stall" ASC ) REFERENCES "dbo"."stalls" ( "id" );
+ALTER TABLE "dbo"."stall_status" ADD CONSTRAINT "contract" NOT NULL FOREIGN KEY ( "id_contract" ASC ) REFERENCES "dbo"."contract" ( "id" );
