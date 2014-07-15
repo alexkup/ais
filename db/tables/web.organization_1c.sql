632abc4c17965b09746aff9d912bcbadd129250e
alexkup <alexkup@mail.ru>
2014-07-15 06:52:13 +0400

    Create web.organization_1c.sql

---
+++ db/tables/web.organization_1c.sql
@@ -0,0 +1,11 @@
+create table web.organization_1c (
+    name nvarchar(255),
+    fullname nvarchar(255),
+    inn nvarchar(255),
+    kpp nvarchar(255),
+    okato nvarchar(255),
+    okpo nvarchar(255),
+    ogrn nvarchar(255),
+    link nvarchar(255),
+    xid UNIQUEIDENTIFIER,
+    primary key (xid));
