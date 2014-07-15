2be1fa0cf01e49d5e578de6b3e4264df55a2aa41
alexkup <alexkup@mail.ru>
2014-07-15 06:55:53 +0400

    Create dbo.objects.sql

---
+++ db/tables/dbo.objects.sql
@@ -0,0 +1,12 @@
+CREATE TABLE dbo.objects (
+	"id" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
+	"address" nvarchar(512) NOT NULL,
+	"number" nvarchar(12) NOT NULL,
+	"coordinates" STPoint NULL,
+	"id_obj_type" integer not null,
+	"xid" UNIQUEIDENTIFIER,
+	PRIMARY KEY ( "id" ASC )
+) IN "system";
+COMMENT ON TABLE "dbo"."objects" IS 'Объекты';
+
+ALTER TABLE "dbo"."objects" ADD CONSTRAINT "obj_type" NOT NULL FOREIGN KEY ( "id_obj_type" ASC ) REFERENCES "dbo"."obj_type" ( "id_obj_type" );
