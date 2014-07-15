3b1ab97b1ee78fa37472b6199382fd5fa0a780d4
alexkup <alexkup@mail.ru>
2014-07-15 07:23:26 +0400

    Create web.rec_stallstype.sql

---
+++ db/proc/web.rec_stallstype.sql
@@ -0,0 +1,17 @@
+create or replace procedure web.rec_stallstype()
+begin
+declare @body long nvarchar;
+set @body=http_body();
+set @body=left(@body,length(@body)-1);
+set @body=right(@body,length(@body)-1);
+truncate table web.stalls_type_1c;
+insert into web.stalls_type_1c
+SELECT
+    name ,link,xid   
+FROM OPENSTRING(value @body) 
+WITH (name nvarchar(255),link nvarchar(255),xid UNIQUEIDENTIFIER)
+option(delimited by '|') 
+as h;
+select 'OK';
+end;
+CREATE SERVICE "stallstype" TYPE 'RAW' AUTHORIZATION OFF USER "web" AS call web.rec_stallstype();
