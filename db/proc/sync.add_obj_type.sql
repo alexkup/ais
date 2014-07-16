f174e0af12dc9632aa93239904795f3b4f23b630
alexkup <alexkup@mail.ru>
2014-07-16 09:53:31 +0400

    Update and rename sync.add_new_obj_type.sql to sync.add_obj_type.sql

--- db/proc/sync.add_obj_type.sql
+++ db/proc/sync.add_obj_type.sql
@@ -1,4 +1,4 @@
-create or replace procedure sync.add_new_obj_type()
+create or replace procedure sync.add_obj_type()
 begin
     insert into dbo.obj_type(name,xid)
     select name,xid from web.obj_type_1c
