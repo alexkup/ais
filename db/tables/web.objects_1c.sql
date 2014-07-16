c501f3327b31be121378b11550bdc05aaefa504f
alexkup <alexkup@mail.ru>
2014-07-16 13:52:35 +0400

    Update web.objects_1c.sql

--- db/tables/web.objects_1c.sql
+++ db/tables/web.objects_1c.sql
@@ -1,6 +1,7 @@
 create table web.objects_1c (
     name nvarchar(255),
     fullname nvarchar(255),
-    type nvarchar(255),
+    typelink nvarchar(255),
     link nvarchar(255),
-    xid UNIQUEIDENTIFIER);
+    xid UNIQUEIDENTIFIER,
+    xid_type UNIQUEIDENTIFIER);
