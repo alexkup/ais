704ac6346f420453e9ea30c03aa4b4655e450dc2
alexkup <alexkup@mail.ru>
2014-07-17 10:40:37 +0400

    Update sync.add_stalls.sql

--- db/proc/sync.add_stalls.sql
+++ db/proc/sync.add_stalls.sql
@@ -4,7 +4,8 @@ begin
  select
     isnull((select top 1 id from dbo.objects where dbo.objects.xid=web.stalls_1c.xid_obj order by dbo.objects.xid),0),
     isnull((select top 1 id_stall_type from dbo.stalls_type where dbo.stalls_type.xid=web.stalls_1c.xid_type order by dbo.stalls_type.xid),0),
-    web.stalls_1c.name
+    web.stalls_1c.name,
+    web.stalls_1c.xid
  from web.stalls_1c
  where web.stalls_1c.xid not in(select xid from stalls)
 end;
