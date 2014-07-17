949392e23e212a35fe2579dd8e3ee3ad734cab84
alexkup <alexkup@mail.ru>
2014-07-17 10:33:41 +0400

    Create sync.add_stalls.sql

---
+++ db/proc/sync.add_stalls.sql
@@ -0,0 +1,10 @@
+create or replace procedure sync.add_stalls()
+begin
+ insert into stalls(id_object,id_stall_type,number,xid)
+ select
+    isnull((select top 1 id from dbo.objects where dbo.objects.xid=web.stalls_1c.xid_obj order by dbo.objects.xid),0),
+    isnull((select top 1 id_stall_type from dbo.stalls_type where dbo.stalls_type.xid=web.stalls_1c.xid_type order by dbo.stalls_type.xid),0),
+    web.stalls_1c.name
+ from web.stalls_1c
+ where web.stalls_1c.xid not in(select xid from stalls)
+end;
