f003d40086cb5eb9d6f43a907ff90c1c4084dc6d
alexkup <alexkup@mail.ru>
2014-07-18 18:58:02 +0400

    Update dbo.logins.sql

--- db/tables/dbo.logins.sql
+++ db/tables/dbo.logins.sql
@@ -3,9 +3,12 @@ create table dbo.logins(
 	username nvarchar(50) not null,
 	pwd nvarchar(100) not null,
 	session_hash nvarchar(100) null,
+	"access" integer NOT NULL,
+	start_page nvarchar(512) NOT NULL,	
 	ts TIMESTAMP NOT NULL DEFAULT TIMESTAMP,
 	PRIMARY KEY ("person_id" ASC)
 ) in system;
 COMMENT ON TABLE "dbo"."logins" IS 'Логины';
 
 ALTER TABLE "dbo"."logins" ADD CONSTRAINT "person" NOT NULL FOREIGN KEY ( "person_id" ASC ) REFERENCES "dbo"."person" ( "person_id" );
+ALTER TABLE "dbo"."logins" ADD CONSTRAINT "access" NOT NULL FOREIGN KEY ( "access" ASC ) REFERENCES "dbo"."access_type" ( "id" );
