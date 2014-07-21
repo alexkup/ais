227e5405edba45e53c63c6060757e50a053efe3d
alexkup <alexkup@mail.ru>
2014-07-21 13:50:10 +0400

    Update web.person_contracts.sql

--- db/proc/web.person_contracts.sql
+++ db/proc/web.person_contracts.sql
@@ -10,12 +10,12 @@ select
     (select name from pers_doc_type pdt where pdt.pers_doc_type_id=pd.pers_doc_type_id)+' '+pd.ser+' '+pd.num+' выдан '+
     pd.org+', '+cast(ddate as nvarchar)
 from pers_doc pd where pd.person_id=person.person_id order by pd.pers_doc_type_id),'|',
-    person.inn,'|'
-    person.xid,'|'
+    person.inn,'|',
+    person.xid,'|',
     (select xid from organization o where o.id=(select id_org from dealership where contract.id_dealership=dealership.id)) as org_xid
      from contract
 left join organization on contract.id_org=organization.id
 left join person on contract.id_person=person.person_id
-left join dealership on dealership.id=contract.id_dealership
 end;
+
 CREATE SERVICE "person_contracts" TYPE 'RAW' USER "sync_1c" AS call web.person_contracts;
