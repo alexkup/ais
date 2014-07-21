d4a3425af45957c0f502895477fdfd7ff3a95410
alexkup <alexkup@mail.ru>
2014-07-21 13:48:05 +0400

    Update web.person_contracts.sql

--- db/proc/web.person_contracts.sql
+++ db/proc/web.person_contracts.sql
@@ -1,17 +1,17 @@
 create or replace procedure web.person_contracts()
 begin
 select 
-    contract.number,
-    contract.ddateb,
-    contract.ddatee,
-    contract.ddatec,
-    (person.lname+' '+person.fname+' '+person.mname),
+    contract.number,'|',
+    contract.ddateb,'|',
+    contract.ddatee,'|',
+    contract.ddatec,'|',
+    (person.lname+' '+person.fname+' '+person.mname),'|',
     (select top 1 
     (select name from pers_doc_type pdt where pdt.pers_doc_type_id=pd.pers_doc_type_id)+' '+pd.ser+' '+pd.num+' выдан '+
     pd.org+', '+cast(ddate as nvarchar)
-from pers_doc pd where pd.person_id=person.person_id order by pd.pers_doc_type_id),
-    person.inn,
-    person.xid,
+from pers_doc pd where pd.person_id=person.person_id order by pd.pers_doc_type_id),'|',
+    person.inn,'|'
+    person.xid,'|'
     (select xid from organization o where o.id=(select id_org from dealership where contract.id_dealership=dealership.id)) as org_xid
      from contract
 left join organization on contract.id_org=organization.id
