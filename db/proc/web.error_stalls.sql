00788ae2844db69f71f20d304af7735af7c80be2
alexkup <alexkup@mail.ru>
2014-07-22 09:03:50 +0400

    Create web.error_stalls.sql

---
+++ db/proc/web.error_stalls.sql
@@ -0,0 +1,13 @@
+create procedure erb.error_stalls()
+as
+begin
+  select name as 'группа мест в 1с',typelink as 'конфликтыне значения','существуют места с различными типами мест' as 'ошибка' from web.objects_1c where xid in(
+    select xid from web.objects_1c
+    where xid_type is not null
+    group by xid
+    having count(xid_type)>1)
+  union all
+  select name,isnull(typelink,'НЕ ЗАДАН!!'),'существуют места с пустыми типами мест' from web.objects_1c where xid in(
+    select xid from web.objects_1c
+    where xid_type is null)
+end;
