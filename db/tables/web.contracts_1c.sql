98ee092664f36d417fa928049c622bf0722b5935
alexkup <alexkup@mail.ru>
2014-07-23 16:15:17 +0400

    Create web.contracts_1c.sql

---
+++ db/tables/web.contracts_1c.sql
@@ -0,0 +1,16 @@
+create table web.contracts_1c(
+  partner_name nvarchar(255),
+  partner_type nvarchar(255),
+  ddateb nvarchar(255),
+  ddatee nvarchar(255),
+  number nvarchar(255),
+  price nvarchar(255),
+  link_org nvarchar(255),
+  link_partner nvarchar(255),
+  link_stall nvarchar(255),
+  link_contract nvarchar(255),
+  xid_org UNIQUEIDENTIFIER,
+  xid_partner UNIQUEIDENTIFIER,
+  xid_stall UNIQUEIDENTIFIER,
+  xid UNIQUEIDENTIFIER,
+  primary key (xid));
