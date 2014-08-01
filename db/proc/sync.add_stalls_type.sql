07b0fb7432118a38456975f00d9c120753ac3c49
alexkup <alexkup@mail.ru>
2014-08-01 14:40:12 +0400

    Update sync.add_stalls_type.sql

--- db/proc/sync.add_stalls_type.sql
+++ db/proc/sync.add_stalls_type.sql
@@ -2,5 +2,5 @@ create or replace procedure sync.add_stalls_type()
 begin
     insert into dbo.stalls_type(name,xid)
     select name,xid from web.stalls_type_1c
-    where web.stalls_type_1c.xid not in(select xid from dbo.stalls_type)
+    where web.stalls_type_1c.xid not in(select xid from dbo.stalls_type where xid is not null)
 end;
