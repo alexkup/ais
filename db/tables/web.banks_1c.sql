64df5b2cbc733705fb5094e2cd1a87d6cafc2927
alexkup <alexkup@mail.ru>
2014-08-11 08:13:35 +0400

    Create web.banks_1c.sql

---
+++ db/tables/web.banks_1c.sql
@@ -0,0 +1,12 @@
+create table web.banks_1c(
+    link nvarchar(255),
+    name nvarchar(255),
+    city nvarchar(255),
+    address nvarchar(255),
+    schet nvarchar(255),
+    korschet nvarchar(255),
+    bik nvarchar(255),
+    lonk_org nvarchar(255),
+    xid_org UNIQUEIDENTIFIER,
+    xid UNIQUEIDENTIFIER,
+    primary key (xid));
