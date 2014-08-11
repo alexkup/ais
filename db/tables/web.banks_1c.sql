fc6d43103a358cf986267cae058f773f0b6d41b0
alexkup <alexkup@mail.ru>
2014-08-11 09:24:06 +0400

    Update web.banks_1c.sql

--- db/tables/web.banks_1c.sql
+++ db/tables/web.banks_1c.sql
@@ -1,12 +1,13 @@
 create table web.banks_1c(
     link nvarchar(255),
     name nvarchar(255),
+    bankname nvarchar(255),
     city nvarchar(255),
     address nvarchar(255),
     schet nvarchar(255),
     korschet nvarchar(255),
     bik nvarchar(255),
-    link_org nvarchar(255),
+    lonk_org nvarchar(255),
     xid_org UNIQUEIDENTIFIER,
     xid UNIQUEIDENTIFIER,
     primary key (xid));
