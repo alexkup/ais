2e94f2f3a7bd239c630e33324ac26483b63b8db4
alexkup <alexkup@mail.ru>
2014-08-11 07:35:27 +0400

    Update dbo.contracts_halted.sql

--- db/tables/dbo.contracts_halted.sql
+++ db/tables/dbo.contracts_halted.sql
@@ -5,3 +5,7 @@ create table dbo.contracts_halted(
     other nvarchar(1024),
     "xid" UNIQUEIDENTIFIER DEFAULT NEWID(),
     PRIMARY KEY (xid));
+
+ALTER TABLE "dbo"."contracts_halted" ADD CONSTRAINT "contract" FOREIGN KEY ( "contract_id" ASC ) REFERENCES "dbo"."contract" ( "id" );
+
+ALTER TABLE "dbo"."contracts_halted" ADD CONSTRAINT "emp" FOREIGN KEY ( "emp_id" ASC ) REFERENCES "dbo"."person" ( "person_id" );
