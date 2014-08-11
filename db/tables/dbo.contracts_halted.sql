f088b6d3a5ff21c6a9282e5bcd540312e8917bbd
alexkup <alexkup@mail.ru>
2014-08-11 08:37:53 +0400

    Update dbo.contracts_halted.sql

--- db/tables/dbo.contracts_halted.sql
+++ db/tables/dbo.contracts_halted.sql
@@ -5,7 +5,8 @@ create table dbo.contracts_halted(
     other nvarchar(1024),
     "xid" UNIQUEIDENTIFIER DEFAULT NEWID(),
     PRIMARY KEY (xid));
+    
+COMMENT ON TABLE "dbo"."contracts_halted" IS 'расторгнутые договра';
 
 ALTER TABLE "dbo"."contracts_halted" ADD CONSTRAINT "contract" FOREIGN KEY ( "contract_id" ASC ) REFERENCES "dbo"."contract" ( "id" );
-
 ALTER TABLE "dbo"."contracts_halted" ADD CONSTRAINT "emp" FOREIGN KEY ( "emp_id" ASC ) REFERENCES "dbo"."person" ( "person_id" );
