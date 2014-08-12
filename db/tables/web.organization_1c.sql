80661de95a14874c54ed58511b3c96fdbfc383ca
alexkup <alexkup@mail.ru>
2014-08-12 15:35:29 +0400

    Update web.organization_1c.sql

--- db/tables/web.organization_1c.sql
+++ db/tables/web.organization_1c.sql
@@ -10,7 +10,7 @@ create table web.organization_1c (
     link_bank nvarchar(255),
     uaddress nvarchar(1024),
     faddress nvarchar(1024),
-    tel nvarchar(256);
+    tel nvarchar(256),
     xid_bank UNIQUEIDENTIFIER,
     xid UNIQUEIDENTIFIER,
     primary key (xid));
