6966b540c39f649283d662b8246780b72f651469
alexkup <alexkup@mail.ru>
2014-08-12 15:34:25 +0400

    Update dbo.organization.sql

--- db/tables/dbo.organization.sql
+++ db/tables/dbo.organization.sql
@@ -11,7 +11,7 @@ CREATE TABLE dbo.organization (
 	"ogrn" nvarchar(60) NULL,
 	uaddress nvarchar(1024),
 	faddress nvarchar(1024),
-	tel nvarchar(50),
+	tel nvarchar(256),
 	"id_bank" integer NOT NULL default 0,
 	"ts" TIMESTAMP NULL DEFAULT TIMESTAMP,
 	"xid" UNIQUEIDENTIFIER  DEFAULT NEWID(),
