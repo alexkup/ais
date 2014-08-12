a83f83cd0ffd545331ee7fe80d417f42f0aeacab
alexkup <alexkup@mail.ru>
2014-08-12 15:50:04 +0400

    Update sync.change_organizations.sql

--- db/proc/sync.change_organizations.sql
+++ db/proc/sync.change_organizations.sql
@@ -7,10 +7,14 @@ begin
         dbo.organization.kpp=web.organization_1c.kpp,
         dbo.organization.okato=web.organization_1c.okato,
         dbo.organization.okpo=web.organization_1c.okpo,
-        dbo.organization.ogrn=web.organization_1c.ogrn
+        dbo.organization.ogrn=web.organization_1c.ogrn,
+        dbo.organization.uaddress=web.organization_1c.uaddress,
+        dbo.organization.faddress=web.organization_1c.faddress,
+        dbo.organization.tel=web.organization_1c.tel
     from web.organization_1c,dbo.organization where (web.organization_1c.xid=dbo.organization.xid) and
-    (web.organization_1c.name<>dbo.organization.name or web.organization_1c.fullname<>dbo.organization.org or
-    web.organization_1c.inn<>dbo.organization.inn or web.organization_1c.kpp<>dbo.organization.kpp or
-    web.organization_1c.okato<>dbo.organization.okato or web.organization_1c.okpo<>dbo.organization.okpo or
-    web.organization_1c.ogrn<>dbo.organization.ogrn)
+    (web.organization_1c.name<>isnull(dbo.organization.name,'') or web.organization_1c.fullname<>isnull(dbo.organization.org,'') or
+    web.organization_1c.inn<>isnull(dbo.organization.inn,'') or web.organization_1c.kpp<>isnull(dbo.organization.kpp,'') or
+    web.organization_1c.okato<>isnull(dbo.organization.okato,'') or web.organization_1c.okpo<>isnull(dbo.organization.okpo,'') or
+    web.organization_1c.ogrn<>isnull(dbo.organization.ogrn,'') or dbo.organization.uaddress<>isnull(web.organization_1c.uaddress,'') or
+    isnull(dbo.organization.faddress,'')<>web.organization_1c.faddress or isnull(dbo.organization.tel,'')<>web.organization_1c.tel)
 end;
