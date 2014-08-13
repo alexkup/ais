d7aef9226c62b422b88b963198471e150f32e9b0
alexkup <alexkup@mail.ru>
2014-08-13 14:56:28 +0400

    Update web.org_contracts.sql

--- db/proc/web.org_contracts.sql
+++ db/proc/web.org_contracts.sql
@@ -14,7 +14,9 @@ select
     organization.okpo,'|',
     organization.ogrn,'|',
     organization.xid,'|',
-    (select xid from organization o where o.id=(select id_org from dealership where contract.id_dealership=dealership.id)) as org_xid,'|'
+    (select xid from organization o where o.id=(select id_org from dealership where contract.id_dealership=dealership.id)) as org_xid,'|',
+    contract.xid,'|',
+    (SELECT list(number) from stall_status join stalls on stalls.id=stall_status.id_stall where stall_status.id_contract=contract.id),'|',char(13)+char(10)
      from contract
 join organization on contract.id_org=organization.id
 left join person on contract.id_person=person.person_id
