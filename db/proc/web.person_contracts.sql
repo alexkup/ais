98a69057ca1b932852c2868e1a61da0fe4f26b4a
alexkup <alexkup@mail.ru>
2014-08-15 16:00:27 +0400

    Update web.person_contracts.sql

--- db/proc/web.person_contracts.sql
+++ db/proc/web.person_contracts.sql
@@ -2,9 +2,9 @@ create or replace procedure web.person_contracts()
 begin
 select 
     contract.number,'|',
-    date(contract.ddateb),'|',
-    date(contract.ddatee),'|',
-    date(contract.ddatec),'|',
+    (contract.ddateb),'|',
+    (contract.ddatee),'|',
+    (contract.ddatec),'|',
     (person.lname+' '+person.fname+' '+person.mname),'|',
     (select top 1 
     (select name from pers_doc_type pdt where pdt.pers_doc_type_id=pd.pers_doc_type_id)+' '+pd.ser+' '+pd.num+' выдан '+
