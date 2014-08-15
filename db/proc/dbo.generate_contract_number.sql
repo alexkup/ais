0bb652e9b5fc3136a44c6624f0d658b0e081a2f1
alexkup <alexkup@mail.ru>
2014-08-15 13:04:20 +0400

    Update dbo.generate_contract_number.sql

--- db/proc/dbo.generate_contract_number.sql
+++ db/proc/dbo.generate_contract_number.sql
@@ -2,10 +2,16 @@ create or replace function dbo.generate_contract_number(@id_stall int)
 returns nvarchar(100)
 begin
   declare @rrr nvarchar(100);
+  declare @k int;
+
+  set @k = (select count(*) from stall_status 
+  join contract on contract.id=stall_status.id_contract
+  where stall_status.id_stall=@id_stall)+1;
+
   set @rrr = (select regexp_substr(obj_type.name,'(\d+)')+rtrim(objects.number)+regexp_substr(stalls.number,'(\d+)')
   from stalls
   join objects on  objects.id=stalls.id_object
   join obj_type on objects.id_obj_type=obj_type.id_obj_type
   where stalls.id=@id_stall);
-    return @rrr;
+    return @rrr+right('00'+cast(@k as nvarchar),3);
 end;
