ace34997c1052a38e5726d229087c944bd62f610
alexkup <alexkup@mail.ru>
2014-08-15 15:25:43 +0400

    Update web.org_contracts.sql

--- db/proc/web.org_contracts.sql
+++ db/proc/web.org_contracts.sql
@@ -16,10 +16,12 @@ select
     organization.xid,'|',
     (select xid from organization o where o.id=(select id_org from dealership where contract.id_dealership=dealership.id)) as org_xid,'|',
     contract.xid,'|',
-    (SELECT list(xid) from stall_status join stalls on stalls.id=stall_status.id_stall where stall_status.id_contract=contract.id),'|',char(13)+char(10)
+    (SELECT list(xid) from stall_status join stalls on stalls.id=stall_status.id_stall where stall_status.id_contract=contract.id),'|',
+    price.cost,'|',char(13)+char(10)
      from contract
 join organization on contract.id_org=organization.id
 left join person on contract.id_person=person.person_id
 left join dealership on dealership.id=contract.id_dealership
+left join price on price.id=contract.id_price
 end;
 CREATE SERVICE "org_contracts" TYPE 'RAW' USER "sync_1c" AS call web.org_contracts;
