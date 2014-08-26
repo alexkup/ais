b8df6d517512c5bf0849c0e0998b1ea6a8fc5d17
alexkup <alexkup@mail.ru>
2014-08-27 02:05:08 +0400

    Update dbo.penalty.sql

--- db/tables/dbo.penalty.sql
+++ db/tables/dbo.penalty.sql
@@ -1,10 +1,13 @@
 create table dbo.penalty(
-    emp_id integer not null,
+    person_id integer not null,
     contract_id integer not null,
     cost DECIMAL,
     other_data nvarchar(200),
     ddate datetime,
     "ts" TIMESTAMP NULL DEFAULT TIMESTAMP,
-	PRIMARY KEY (emp_id,contract_id ASC) 
+	PRIMARY KEY (person_id,contract_id ASC) 
 ) IN "system";
-COMMENT ON TABLE "dbo"."price" IS 'Штрафы'
+COMMENT ON TABLE "dbo"."penalty" IS 'Штрафы'
+
+ALTER TABLE "dbo"."penalty" ADD CONSTRAINT "contract" NOT NULL FOREIGN KEY ( "contract_id" ASC ) REFERENCES "dbo"."contract" ( "id" );
+ALTER TABLE "dbo"."penalty" ADD CONSTRAINT "person" NOT NULL FOREIGN KEY ( "person_id" ASC ) REFERENCES "dbo"."person" ( "person_id" );
