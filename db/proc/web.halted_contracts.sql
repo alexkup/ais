cde93b8cae8f8b4cc580b6e8268e59810b1fe6d3
alexkup <alexkup@mail.ru>
2014-08-26 22:29:50 +0400

    Create web.halted_contracts.sql

---
+++ db/proc/web.halted_contracts.sql
@@ -0,0 +1,10 @@
+create or replace procedure web.halted_contracts()
+begin
+select  contract.xid,'|',
+        contracts_halted.xid,'|',
+        contracts_halted.xid_act,'|',char(13)+char(10)
+ from contracts_halted
+join contract on contract.id=contracts_halted.contract_id
+end;
+
+CREATE SERVICE "halted_contracts" TYPE 'RAW' USER "sync_1c" AS call web.halted_contracts;
