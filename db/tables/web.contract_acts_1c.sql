843610da0996fbb0f9ad586030f104cb8ac4f764
alexkup <alexkup@mail.ru>
2014-10-08 10:01:51 +0400

    Create web.contract_acts_1c.sql

---
+++ db/tables/web.contract_acts_1c.sql
@@ -0,0 +1,7 @@
+create table web.contract_acts_1c(
+  link_contract nvarchar(255),
+  link_zak nvarchar(255),
+  link_act nvarchar(255),
+  xid_contrcat UNIQUEIDENTIFIER,
+  xid_zak UNIQUEIDENTIFIER,
+  xid_act UNIQUEIDENTIFIER);
