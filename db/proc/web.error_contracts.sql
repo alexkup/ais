271fca052613583b5907dd11a8e7fc72b24abcc5
alexkup <alexkup@mail.ru>
2014-10-08 14:14:07 +0400

    Create web.error_contracts.sql

---
+++ db/proc/web.error_contracts.sql
@@ -0,0 +1,16 @@
+create or replace procedure web.error_contracts()
+as
+begin
+select 
+    number as 'Номер догвора',
+    ddateb as 'Дата начала',
+    ddatee as 'Дата окончания',
+    ddatec as 'Дата создания',
+    other_data as 'Контрагент(порчее)'
+from contract
+left join web.contract_acts_1c on web.contract_acts_1c.xid_contrcat=contract.xid
+where web.contract_acts_1c.xid_contrcat is null
+order by other_data
+end;
+
+CREATE SERVICE "error_contracts" TYPE 'HTML' AUTHORIZATION OFF USER "web" AS call web.error_contracts();
