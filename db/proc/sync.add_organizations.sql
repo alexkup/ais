8267dc6b24d5fbbccd8ee54c9787ee5291ef9ef9
alexkup <alexkup@mail.ru>
2014-10-13 08:03:40 +0400

    Update sync.add_organizations.sql

--- db/proc/sync.add_organizations.sql
+++ db/proc/sync.add_organizations.sql
@@ -1,7 +1,7 @@
 create or replace procedure sync.add_organizations()
 begin
    
-    insert into dbo.organization(name,org,inn,kpp,okato,okpo,ogrn,uaddress,faddress,tel)
+    insert into dbo.organization(name,org,inn,kpp,okato,okpo,ogrn,uaddress,faddress,tel,xid)
     select
         web.organization_1c.name,
         web.organization_1c.fullname,
@@ -12,7 +12,8 @@ begin
         web.organization_1c.ogrn,
         web.organization_1c.uaddress,
         web.organization_1c.faddress,
-        web.organization_1c.tel
+        web.organization_1c.tel,
+        web.organization_1c.xid
     from web.organization_1c where web.organization_1c.xid not in(select xid from dbo.organization where xid is not null)
 
 end;
