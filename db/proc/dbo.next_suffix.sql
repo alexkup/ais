2fd65606f3cc9041876546a953a4d2651ccc9bf0
alexkup <alexkup@mail.ru>
2014-10-15 08:33:46 +0400

    Update dbo.next_suffix.sql

--- db/proc/dbo.next_suffix.sql
+++ db/proc/dbo.next_suffix.sql
@@ -7,5 +7,5 @@ set @k=cast((select count(lname+' '+mname+' '+fname) from person
     where (lname+' '+mname+' '+fname)=@fio
     group by (lname+' '+mname+' '+fname)) as int); 
  
-return @k-1;
+return @k;
 end;
