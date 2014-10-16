adaef1055ebf795bf257f02670d52d39dfb04658
alexkup <alexkup@mail.ru>
2014-10-16 07:32:25 +0400

    Create dbo.stall_contracts_now.sql

---
+++ db/views/dbo.stall_contracts_now.sql
@@ -0,0 +1,25 @@
+create view dbo.stall_contracts_now as
+select 
+    contract.id,
+	contract.number,
+	contract.id_template,
+	contract.ddateb,
+	contract.ddatee,
+	contract.ddatec,
+	contract.id_picture,
+	contract.id_org,
+	contract.id_person,
+	contract.id_dealership,
+	contract.id_dealership_emp,
+	contract.id_price,	
+	contract.signature,
+	contract."1c_flag",
+	contract.other_data,
+	contract.xid,
+	contract.xid_act,
+	contract.xid_act2,
+	contract.ts,
+	contract.want_halt
+ from contract
+join stall_status on stall_status.id_contract=contract.id
+where (getdate() between contract.ddateb and contract.ddatee)
