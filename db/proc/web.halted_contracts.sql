4ea260ac12d1b705da7151dc13d1420e05f2db3b
alexkup <alexkup@mail.ru>
2014-10-16 08:13:35 +0400

    Update web.halted_contracts.sql

--- db/proc/web.halted_contracts.sql
+++ db/proc/web.halted_contracts.sql
@@ -6,6 +6,7 @@ select  contract.xid_act,'|',
         contracts_halted.xid_act,'|',char(13)+char(10)
  from contracts_halted
 join contract on contract.id=contracts_halted.contract_id
+where contract."1c_flag"=1
 end;
 
 CREATE SERVICE "halted_contracts" TYPE 'RAW' USER "sync_1c" AS call web.halted_contracts;
